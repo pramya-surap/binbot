bf09eefd086d428b0f297b3c344e491dfee84a2e
pramya-surap <144474299+pramya-surap@users.noreply.github.com>
2024-07-17 23:43:27 -0400

    web cam

---
+++ webcamgpt/utils.py
@@ -0,0 +1,37 @@
+import cv2
+import base64
+import numpy as np
+
+
+def encode_image_to_base64(image: np.ndarray) -> str:
+    success, buffer = cv2.imencode('.jpg', image)
+    if not success:
+        raise ValueError("fail.")
+
+    encoded_image = base64.b64encode(buffer).decode('utf-8')
+    return encoded_image
+
+
+def compose_payload(image: np.ndarray, prompt: str) -> dict:
+    base64_image = encode_image_to_base64(image)
+    return {
+        "model": "gpt-4o",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": prompt
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
+                    }
+                ]
+            }
+        ],
+        "max_tokens": 300
+    }
